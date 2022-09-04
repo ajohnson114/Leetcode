54b99b29ae6c0b11a740405d67a70bdcf84d3ae3
ajohnson114 <67026227+ajohnson114@users.noreply.github.com>
2022-09-04 17:02:59 -0400

    Update ValidParentheses.py

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -1,7 +1,7 @@
 class Solution:
     def isValid(self, s: str) -> bool:
       """
-      The key to solving this is by using a stack. We know that parentheses must be closes immediately so we can initialze a dictionary with keys as closing
+      The key to solving this is by using a stack. We know that parentheses must be closed immediately so we can initialze a dictionary with keys as closing
       parentheses and values as open parentheses. Then we iterate through the string and append all the open parentheses and check if the value we're 
       currently on is a closing parenthesis. If it is we should check that the stack isn't empty and it is closing something and that the thing it is closing
       is its corresponding pair which we can check by querying the dictionary we made. Thats it!
