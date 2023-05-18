1fe59d09dc47fc7ccba496927787477488dd9c00
ajohnson114 <67026227+ajohnson114@users.noreply.github.com>
2023-05-18 06:41:27 -0400

    Update LongestCommonPrefix.py

--- LongestCommonPrefix.py
+++ LongestCommonPrefix.py
@@ -7,6 +7,11 @@ def longestCommonPrefix(self, strs: List[str]) -> str:
         strs = ["abc", "def", "ghi"]  list(zip(*strs)) = [('a', 'd', 'g'), ('b', 'e', 'h'), ('c', 'f', 'i')]
         Then you iterate through list(zip(*strs)) and see if the length of the set is equal to 1 and if it is then we 
         append to the result and if it's not we break as the prefix is now different. At the end we just join the prefix
+        
+        Note: the zip function will stop making tuples when there isn't an equal amount of things to join i.e. it stops
+        at the length of the shortest word in this case. For instance the following will have the same output as above
+        strs = ["abc", "defz", "ghiy"]  list(zip(*strs)) = [('a', 'd', 'g'), ('b', 'e', 'h'), ('c', 'f', 'i')]
+        
         """
         prefix=[]
         num = len(strs)
